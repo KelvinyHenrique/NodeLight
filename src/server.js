9b1573521c18d373e7e4f11b8a2f1bd9d69c14da
kelvinyhenrique <kelvinyhenrique17@gmail.com>
2023-04-13 22:03:15 -0300

    Basic version of light control

--- src/server.js
+++ src/server.js
@@ -4,15 +4,31 @@ const http = require("http");
 const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const mqtt = require("mqtt");
+
 const io = new Server(server);
+
+const client = mqtt.connect("mqtt://broker.emqx.io:1883");
+let mqttConnected = false;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+client.on("connect", function () {
+  client.subscribe("qwgsdfgf8672348602436qe/office/light", function (err) {
+    if (!err) {
+      console.log("connected to mqtt");
+    }
+  });
+  mqttConnected = true;
+});
+
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    if (mqttConnected) {
+      client.publish("qwgsdfgf8672348602436qe/office/light", msg);
+    }
   });
 });
 
